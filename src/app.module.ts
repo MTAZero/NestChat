0c3e7613f6f95ad237d84e668d1bff69c25a6516
admin <admin@MacBook-Pro-cua-admin.local>
2021-02-07 16:43:07 +0700

    Chỉnh sửa cấu trúc thư mục

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { CatModule } from './cat/cat.module';
 import { TestModule } from './modules/test/test.module';
 import { AuthModule } from './modules/auth/auth.module';
 
 @Module({
-  imports: [CatModule, TestModule, AuthModule],
+  imports: [TestModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
